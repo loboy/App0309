b178637fd83ea3e81089bc253489fd9a9ff772df
loboy <loboyoh@pchome.com.tw>
2016-03-11 16:49:37 +0800

    CheckBox to hide TextView
    CheckBox to hide TextView

--- ClassUI/app/src/main/java/com/example/test/classui/MainActivity.java
+++ ClassUI/app/src/main/java/com/example/test/classui/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.test.classui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View; // For use of void submit(View view)
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     // ? 根據XML LAYOUT的物件 ID來先行宣告，稍後會於程式碼中取得正確的值!
     TextView textView;
     EditText editText;
+    CheckBox checkBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +24,26 @@ public class MainActivity extends AppCompatActivity {
         // 習慣上，JAVA這邊宣告的物件名稱會故意命名為跟XML的物件ID名稱相同，方便程式開發者了解其對應關係!!!
         textView = (TextView) findViewById(R.id.textView);
         editText = (EditText) findViewById(R.id.editText);
+        checkBox = (CheckBox) findViewById(R.id.checkBox);
     }
 
     public void submit(View view)
     {
         //textView.setText("Hello~ Welcome!!!");
-        Toast.makeText(this, "Hello~Welcome!!!",Toast.LENGTH_LONG).show();
+        //Toast.makeText(this, "Hello~Welcome!!!",Toast.LENGTH_LONG).show();
 
         String text = editText.getText().toString();   //宣告一個local variable text，值為EditText物件 editText的內容 (APP執行時，由使用者所輸入的內容)
-        textView.setText(text);  //將TextView物件textView內容改成 區域變數text的內容!
+        Toast.makeText(this, "Hello~Welcome:" + text ,Toast.LENGTH_LONG).show();
+
+        if(checkBox.isChecked()) // 判斷 CheckBox物件 checkBox 是否為 勾選狀態 (True / False)
+        {
+            textView.setText("*********");  //將TextView物件textView內容改成 顯示*********
+        }
+        else
+        {
+            textView.setText(text);  //將TextView物件textView內容改成 區域變數text的內容!
+        }
+
         editText.setText("");  //將EditText物件 editText的內容(使用者輸入的內容)清空
     }
 }
