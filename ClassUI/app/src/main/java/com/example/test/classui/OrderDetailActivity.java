83b043fd1a1b971bf53568772891935b9b1fc6e1
loboy <loboyoh@pchome.com.tw>
2016-04-04 16:44:30 +0800

    Photo Url process and Google Map URL request
    Get the point of Lat & Lng associated with Address
    Get the map url by the center point and zoom
    Switch to change the show of ImageView or WebView

--- ClassUI/app/src/main/java/com/example/test/classui/OrderDetailActivity.java
+++ ClassUI/app/src/main/java/com/example/test/classui/OrderDetailActivity.java
@@ -1,10 +1,18 @@
 package com.example.test.classui;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
@@ -16,10 +24,19 @@ import org.json.JSONObject;
 
 public class OrderDetailActivity extends AppCompatActivity {
 
+    // for photo URL from Parse Server
+    private String photoURL;
+
+    //for address extracted from Parse Server
+    private String address;
+
     TextView noteDetail;
     TextView storeDetail;
     TextView menuDetail;
     ImageView photoDetail;
+    ImageView mapImageView;
+    WebView mapWebView;
+    Switch imageWebViewSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +47,21 @@ public class OrderDetailActivity extends AppCompatActivity {
         storeDetail = (TextView) findViewById(R.id.storeDetail);
         menuDetail = (TextView) findViewById(R.id.menuDetail);
         photoDetail = (ImageView) findViewById(R.id.photoDetail);
+        mapImageView = (ImageView) findViewById(R.id.mapImageView);
+        mapWebView = (WebView) findViewById(R.id.mapWebView);
+        imageWebViewSwitch = (Switch) findViewById(R.id.imageWebViewSwitch);
+        imageWebViewSwitch.setChecked(false);
+
+
+        // ImageView and WebView Default Setting of Visible
+        mapImageView.setVisibility(View.VISIBLE);
+        mapWebView.setVisibility(View.GONE);
 
         noteDetail.setText(getIntent().getStringExtra("note"));
-        storeDetail.setText(getIntent().getStringExtra("storeInfo"));
+
+        String storeInformation = getIntent().getStringExtra("storeInfo");
+        storeDetail.setText(storeInformation); //storeDetail.setText(getIntent().getStringExtra("storeInfo"));
+        address = storeInformation.split(",")[1]; //取出 店家的地址
 
         //處理menu
         String menu = getIntent().getStringExtra("menu");
@@ -61,11 +90,96 @@ public class OrderDetailActivity extends AppCompatActivity {
         }
 
         //處理圖像
-        String photoURL = getIntent().getStringExtra("photoURL");
+        photoURL = getIntent().getStringExtra("photoURL");
         if(photoURL != null)
         {
-            // 利用  import com.squareup.picasso.Picasso;   使用套件幫忙輕鬆處理圖片顯示   // 須加上設定 compile 'com.squareup.picasso:picasso:2.5.2'
-            Picasso.with(this).load(photoURL).into(photoDetail);
+            /*
+                            // 利用  import com.squareup.picasso.Picasso;   使用套件幫忙輕鬆處理圖片顯示   // 須加上設定 compile 'com.squareup.picasso:picasso:2.5.2'
+                            Picasso.with(this).load(photoURL).into(photoDetail);
+                         */
+
+            // Self Implement for Photo Show // photoURL為ParseServer上的圖片資源；photoDetail為欲顯示圖片的ImageView
+            // (new ImageLoad(photoDetail)).execute(photoURL);
+            (new ImageLoad(photoDetail)).execute(photoURL); // 需要使用new 宣告物件，才能在同一行以.execute寫法執行
+        }
+
+        //處理地圖顯示
+        if(address != null) //如何判斷addresss是個正確合理、GoogleMAP能定位的地址?
+        {
+            (new GeoMap()).execute(address);
+        }
+
+        //處理Switch切換顯示ImageView 或 WebView
+        imageWebViewSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) // 只顯示 WebView
+                {
+                    mapImageView.setVisibility(View.GONE); // ImageView設為Gone: 不顯示且不佔預留版面空間
+                    mapWebView.setVisibility(View.VISIBLE);
+                    Toast.makeText(OrderDetailActivity.this, "Switch is-Checked State!!! ", Toast.LENGTH_LONG).show();
+                }
+                else // 只顯示 ImageView
+                {
+                    mapWebView.setVisibility(View.GONE);
+                    mapImageView.setVisibility(View.VISIBLE);
+                    Toast.makeText(OrderDetailActivity.this, "Switch un-Checked State!!! ", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    // For Image Load in OrderDetailActivity
+    class ImageLoad extends AsyncTask<String, Void, byte[]>
+    {
+        ImageView imageView;
+
+        public ImageLoad(ImageView imageView)
+        {
+            this.imageView = imageView; // 將傳入參數的imageView 存入此class下的ImageView物件
+        }
+
+        @Override
+        protected byte[] doInBackground(String... params)
+        {
+            String url = params[0]; //傳入參數陣列中的第一個參數
+            return Utils.urlToByte(url); // 透過自訂Utils class的urlToByte()將URL轉成byte[]
+        }
+
+        @Override
+        protected void onPostExecute(byte[] bytes)
+        {
+            Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+            imageView.setImageBitmap(bmp);
+            // super.onPostExecute(bytes);
+        }
+    }
+
+    //以地址Address向Google MAP API取得地圖資訊並顯示
+    class GeoMap extends AsyncTask<String, Void, byte[]>
+    {
+        private double[] latLng;
+        private String mapUrl;
+
+        @Override
+        protected byte[] doInBackground(String... params)
+        {
+            latLng = Utils.addressToLatLng(params[0]);
+            mapUrl = Utils.getGoogleMapUrl(latLng, 17);
+            return Utils.urlToByte(mapUrl);
+        }
+
+        @Override
+        protected void onPostExecute(byte[] bytes)
+        {
+            super.onPostExecute(bytes); // 有沒有都沒關係!
+
+            //Shown in ImageView
+            Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+            mapImageView.setImageBitmap(bmp);
+
+            //Shown in WebView
+            mapWebView.loadUrl(mapUrl);
         }
     }
 }
--- ClassUI/app/src/main/java/com/example/test/classui/Utils.java
+++ ClassUI/app/src/main/java/com/example/test/classui/Utils.java
@@ -3,12 +3,23 @@ package com.example.test.classui;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Environment;
+import android.util.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 
 /**
  * Created by Loboy on 2016/3/17.
@@ -70,5 +81,102 @@ public class Utils {
 
         //回傳該檔名的圖檔Uri
         return Uri.fromFile(classImageFile);
+    }
+
+    public static byte[] urlToByte(String urlString)
+    {
+        try {
+            URL url = new URL(urlString);
+            URLConnection urlConnection = url.openConnection();
+            InputStream is = urlConnection.getInputStream();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream(); // 串流輸入的目的地
+            byte[] buffer = new byte[1024];
+            int len = 0;
+            // 將 InputStream 利用 buffer_byte[1024] 轉存入 ByteArrayOutputStream， 簡寫如下
+            while( (len=is.read(buffer)) != -1 ) // != -1 表示還有資料
+            {
+                baos.write(buffer, 0, len);
+            }
+            return  baos.toByteArray();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        //如果try-catch 偵測到錯誤
+        return null;
+    }
+
+    // 轉成符合utf-8格式的Google API url
+    public static String getGeocodeEncodeUrl(String address)
+    {
+        try
+        {
+            address = URLEncoder.encode(address, "utf-8");
+        }
+        catch (UnsupportedEncodingException e)
+        {
+            e.printStackTrace();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        String googleApiUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + address;
+        return googleApiUrl;
+    }
+
+    //將URLConnection取回的byte[]轉成String之後，可視為JSONObject處理，從眾多資訊中取得經緯度座標
+    public static double[] getLatLngFromJsonString(String jsonString)
+    {
+        try
+        {
+            JSONObject obj = new JSONObject(jsonString);
+            if(!obj.getString("status").equals("OK"))
+            {
+                return null;
+            }
+            JSONObject location = obj.getJSONArray("results")
+                    .getJSONObject(0)
+                    .getJSONObject("geometry")
+                    .getJSONObject("location");
+            double lat = location.getDouble("lat");
+            double lng = location.getDouble("lng");
+            Log.d("Debug", "JSON_String:" + jsonString);
+            Log.d("Debug", "Lat:" + lat);
+            Log.d("Debug", "Lng:" + lng);
+            return new double[]{lat, lng}; //經緯度座標
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    //將String地址轉為double[]經緯度座標
+    public static double[] addressToLatLng(String address)
+    {
+        String url = Utils.getGeocodeEncodeUrl(address); // 符合utf-8 Encode的URL
+        byte[] bytes = Utils.urlToByte(url); //透過URLConnection取得JSON格式的Byte Array
+        String jsonResult = new String(bytes); //將byte[] 轉成String
+        double[] latLngLocation = Utils.getLatLngFromJsonString(jsonResult); //從JSONString中擷取經緯度座標
+        return latLngLocation;
+    }
+
+    public static String getGoogleMapUrl(double[] latLng, int zoom)
+    {
+        String center = latLng[0] + "," + latLng[1];
+        String mapUrl = "https://maps.googleapis.com/maps/api/staticmap?center=" + center + "&zoom=" + zoom + "&size=640x480";
+        return mapUrl;
     }
 }
